2a5865c062dd4638ae4c1b298814e5f8d37bf36c
Tom Villegas <tvillega@dcc.uchile.cl>
2023-10-01 19:00:40 -0300

    style(install): add quotes for gobbling safety

--- install.sh
+++ install.sh
@@ -3,20 +3,26 @@
 # Create bin directory
 
 if [[ ! -d "$HOME/.local/bin" ]] ; then
-    mkdir -p $HOME/.local/bin
+    mkdir -p "$HOME/.local/bin"
 fi
 
 # Add to .bashrc
 
+## Create .bashrc.bak just to be sure
+cp "$HOME/.bashrc" "$HOME/.bashrc.bak"
+
 if [[ ! ${PATH%%:*} == "$HOME/.local/bin" ]] ; then
-    printf -- "[[ -d \"\$HOME/.local/bin\" ]] && PATH=\"\$HOME/.local/bin:\$PATH\"" >> .bashrc
+    printf -- "[[ -d \"\$HOME/.local/bin\" ]] && PATH=\"\$HOME/.local/bin:\$PATH\"" >> "$HOME/.bashrc"
 fi
 
 # Add to .profile
 
+## Create .profile.bak just to be sure
+cp "$HOME/.profile" "$HOME/.profile.bak"
+
 ## Create file if it does not exist
 if [[ ! -f "$HOME/.profile" ]] ; then
-    touch .profile
+    touch "$HOME/.profile"
 fi
 
 ## Search for the string on the file
@@ -24,27 +30,27 @@ grep "source \$HOME/.bashrc" .profile > /dev/null 2>&1
 
 ## If not found (exit code 1), append it
 if [[ $? -eq 1 ]] ; then # Not found
-    echo "source \$HOME/.bashrc" >> $HOME/.profile
+    echo "source \$HOME/.bashrc" >> "$HOME/.profile"
 fi
 
 # Create src directory
 
 if [[ ! -d "$HOME/.local/src" ]] ; then
-    mkdir -p $HOME/.local/src
+    mkdir -p "$HOME/.local/src"
 fi
 
 # Clone repo
 
 if [[ -d "$HOME/.local/src/dcc-tools" ]] ; then
-    rm -rf $HOME/.local/src/dcc-tools
+    rm -rf "$HOME/.local/src/dcc-tools"
 fi
 
-git clone -b ssh-client-mode https://github.com/tvillega/dcc-tools.git $HOME/.local/src/dcc-tools
+git clone -b ssh-client-mode https://github.com/tvillega/dcc-tools.git "$HOME/.local/src/dcc-tools"
 
 # Enable bin
 
 if [[ -f ${HOME}/.local/bin/dcc-tools ]] ; then
-    rm $HOME/.local/bin/dcc-tools
+    rm "$HOME/.local/bin/dcc-tools"
 fi
 
-ln -s ${HOME}/.local/src/dcc-tools/dcc-tools ${HOME}/.local/bin/dcc-tools
+ln -s "$HOME/.local/src/dcc-tools/dcc-tools" "$HOME/.local/bin/dcc-tools"
